552423cdd82b591672ca3406a2f36b9d2f4cdf7e
Alquipo Neto <alquiponeto@outlook.com.br>
2022-09-13 12:09:14 -0300

    fix: post data

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,41 @@ import { Sidebar } from "./components/Sidebar"
 import styles from "./App.module.css"
 import "./global.css"
 
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: "https://github.com/alquipo.png",
+      name: "Alquipo Neto",
+      role: "Web Developer"
+    },
+    content: [
+      { type: "paragraph", content: "Fala galeraa 👋" },
+      { type: "paragraph", content: "Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀" },
+      { type: "link", content: " jane.design/doctorcare" },
+
+
+    ],
+    publishedAt: new Date("2022-09-12 12:00:00")
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: "https://github.com/alquipo.png",
+      name: "Alquipo Neto 2",
+      role: "Web Developer 2"
+    },
+    content: [
+      { type: "paragraph", content: "Fala galeraa 👋" },
+      { type: "paragraph", content: "Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀" },
+      { type: "link", content: " jane.design/doctorcare" },
+
+
+    ],
+    publishedAt: new Date("2022-09-10 12:00:00")
+  },
+]
+
 function App() {
 
   return (
@@ -16,7 +51,15 @@ function App() {
 
         <Sidebar />
         <main>
-          <Post />
+          {posts.map(post => (
+
+            <Post
+              author={post.author}
+              content={post.content}
+              publishedAt={post.publishedAt}
+            />
+          )
+          )}
         </main>
       </div>
 
